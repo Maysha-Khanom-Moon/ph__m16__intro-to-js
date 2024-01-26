cda1a4c7f06e119d7276cd2b4d5c49a4d2b77105
Maysha-Khanom-Moon <maysha412@gmail.com>
2024-01-26 14:35:10 +0600

    basic math needed for web development

---
+++ math.js
@@ -0,0 +1,17 @@
+/**
+ *********************************************************
+ *              MATH NEEDED FOR PROGRAMMING  
+ *********************************************************
+ * 1. Number: +ve, -ve, 0 [59, -9, 0]
+ * 2. integer: 2, 9, [odd, even]
+ * 3. float: 5.6, 2.2
+ * 4. Operation: +, -, *, /
+ * 5. More operation: %, ++, --
+ * 6. percentage: 5%, 10% of 50 tk
+ * 7. Sum: 14, 18, 16
+ * 8. Avg: 14, 18, 16
+ * 9. Max/Min: 18, 16, 14
+ * 10. Series: 1 to n[integer, odd, even, square]
+ * 11. Area: Triangle, Square, Circle
+ * 12. Prime number, Permutation, Graph
+ */
