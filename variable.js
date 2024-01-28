74d11191a0996b0e8bdff54afb0cc98ed7fc45c5
Maysha-Khanom-Moon <maysha412@gmail.com>
2024-01-28 14:44:38 +0600

    basic of variable and var

---
+++ variable.js
@@ -0,0 +1,28 @@
+/**
+ * ** variable: we alocate some storage via name and type and this storage can used via variable name.
+ * 
+ * VARIABLE ==> vary + able
+ * 
+ * ## 5 things to declare a variable
+ *      1. keywords(var/ let/ const)
+ *      2. variable name
+ *      3. '=' to asign value
+ *      4. variable value
+ *      5. ';'
+ */
+
+var weight = 38;
+var price = 38;
+var year = 2005;
+
+var money = 4500;
+console.log(money);
+
+var age; // undefined
+console.log(age);
+
+
+
+// change the value of variable
+money = 10000;
+console.log(money);
