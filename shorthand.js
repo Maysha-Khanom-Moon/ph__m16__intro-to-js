40b5967334bd8e4f925fc9fb7545db333e6b34e2
Maysha-Khanom-Moon <maysha412@gmail.com>
2024-01-28 19:57:35 +0600

    js concetanation and shorthand mathematical term

---
+++ shorthand.js
@@ -0,0 +1,33 @@
+var price = 35;
+
+price += 10; // price = price + 10
+console.log(price);
+
+
+price -= 20; // price = price -20
+console.log(price);
+
+
+price *= 2; // price = price * 2
+console.log(price);
+
+
+price /= 2; // price = price / 2
+console.log(price);
+
+
+price %= 2; // price = price % 2
+console.log(price + '\n');
+
+
+// ****** concetanation ******
+// anything + string = string ---> pasha-pashi boshbe
+
+console.log(4 + 2);
+console.log(4+'2');
+
+// but
+console.log(4 - '3');
+console.log(4 * '3');
+console.log(4 / '3');
+console.log(4 % '3');
