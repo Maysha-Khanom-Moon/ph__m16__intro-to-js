729321e9232389b1a6254f81a86105f0d9f7458d
Maysha-Khanom-Moon <maysha412@gmail.com>
2024-01-28 15:02:00 +0600

    string, number, boolean

---
+++ var_types.js
@@ -0,0 +1,29 @@
+// var name = mkmoon; 
+// name think mkmoon a name of another variable. To make it value we have to use '""' or "''".
+
+
+// These are string
+var name = "Maysha Khanom Moon";
+console.log(name);
+
+var country = 'Bangladesh';
+console.log(country);
+
+
+// boolean
+var isRich = true;
+console.log(isRich);
+
+var passed = false;
+console.log(passed);
+
+
+// number
+var money = 4500;
+console.log(money);
+
+
+// to check the type of value
+console.log(typeof(country));
+console.log(typeof passed);
+console.log(typeof money);
