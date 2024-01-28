dae7552a81e0d26401fa3de14833de194aed3df6
Maysha-Khanom-Moon <maysha412@gmail.com>
2024-01-28 19:48:04 +0600

    mathametical opetations

---
+++ operation.js
@@ -0,0 +1,24 @@
+// addition: +
+var eggPrice = 10;
+var onionPrice = 20;
+console.log(eggPrice + onionPrice);
+
+
+// subtraction: -
+console.log(onionPrice - eggPrice);
+
+
+// multiplication: *
+console.log(onionPrice * eggPrice);
+
+
+// division: /
+console.log(onionPrice / eggPrice);
+console.log(eggPrice / 3); // yellow (number)
+console.log(parseInt(eggPrice / 3));
+console.log((eggPrice / 3).toFixed(2)); // white (string)
+
+
+// remainder or modulo: %
+console.log(onionPrice % eggPrice);
+console.log(eggPrice % onionPrice); 
