ca38398eed11bd738c7931b472ea0d25dd3be4a4
Maysha-Khanom-Moon <maysha412@gmail.com>
2024-01-28 15:19:30 +0600

    naming convention of js and variable

---
+++ naming.js
@@ -0,0 +1,46 @@
+/**
+ * ********* js is case sensitive *********
+ * 
+ * ********** naming convention of variable **********
+ *      ==> camel case
+ */
+
+// 1. no keyword in variable name
+var false = 45;  // wrong
+var False = 45; // write
+
+
+// 2. no space or gap in variable name
+var is happy = false;
+var isHappy = true;
+
+
+// 3. no quote
+var "address" = "talaimari, rajshahi";
+var address = "talaimari, rajshahi"; 
+
+
+// 4. can not start with a number. but number other than the is allowed
+var 1money = 45;
+var money1 = 45;
+var mon1ey = 45;
+
+
+
+// 5. name is case sensitive
+var address;
+var Address;
+// they are not same
+
+
+
+// 6. how to write a long variable name
+var my current location = 'talaimari, rajshahi';
+var mycurrentlocation = 'talaimari, rajshahi'; // not readable
+var my_current_location = 'talaimari, rajshahi'; // snake case
+var myCurrentLocation = 'talaimari, rajshahi'; // camel case
+var MyCurrentLocation = 'talaimari, rajshahi'; // pascal case
+
+
+// we can't use any special character
+// just '_' can be used
